f27d770cf90dc21bf840abcf04ee62aa2c8fdb19
kevincholopez <ingkevinlopez@outlook.com>
2024-02-03 20:41:22 -0500

    ajustar pagina principla para headers y deploy

--- main.py
+++ main.py
@@ -4,12 +4,27 @@ from config.database import engine, Base
 from middlewares.error_handler import ErrorHandler
 from routers.movie import movie_router
 from routers.user import user_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origin=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 app.title = "Mi aplicación con  FastAPI"
 app.version = "0.0.1"
 
 app.add_middleware(ErrorHandler)
+
+@app.get("/")
+def read_root():
+    return "Api de adopta la plata huila"
 
 app.include_router(movie_router)
 app.include_router(user_router)
